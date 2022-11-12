b614fdcd34837db417584a92f352101f6b1654be
Gaurav <gangulygaurav166@gmail.com>
2022-11-13 00:26:43 +0530

    Day-5

---
+++ Favourite.js
@@ -0,0 +1,143 @@
+let favArr = JSON.parse(localStorage.getItem("Fav-Item"))||[];
+displayFav(favArr);
+
+
+function displayFav(Data){
+    document.querySelector(".container").innerHTML = null;
+
+    if(favArr.length == 0){
+        let div = document.createElement("div");
+        let div1 = document.createElement("div");
+        let span = document.createElement("span");
+        let h1 = document.createElement("h1");
+        let div2 = document.createElement("div");
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        let a = document.createElement("a");
+
+        div.setAttribute("class","FavEmpty");
+        span.setAttribute("class","material-symbols-outlined")
+        span.textContent = "favorite";
+        h1.textContent = "You have no Saved Items";
+        p1.textContent = "Sign in to sync your Saved Items";
+        p2.textContent = "across all your devices.";
+        a.textContent = "SIGN IN";
+        a.setAttribute("href","./LogIn.html");
+
+        div2.append(p1,p2);
+        div1.append(span,h1,div2,a);
+        div.append(div1);
+        document.querySelector(".container").append(div);
+    }
+    else{
+        let div = document.createElement("div");
+        div.setAttribute("class","favFull");
+
+        favArr.forEach((item,index)=>{
+
+            let div1 = document.createElement("div");
+            let image = document.createElement("img");
+            let span = document.createElement("span");
+            let p1 = document.createElement("p");
+            let p2 = document.createElement("p");
+            let p3 = document.createElement("p");
+
+            span.textContent = "delete";
+            span.setAttribute("class","material-symbols-outlined");
+            image.setAttribute("src",item.image);
+            p1.textContent = item.des;
+            p2.textContent = "£"+item.price;
+            p3.textContent = "Perfect";
+
+    
+            /*select div*/
+            let div3 = document.createElement("div");
+            let select = document.createElement("select");
+            let option1 = document.createElement("option");
+            let option2 = document.createElement("option");
+            let option3 = document.createElement("option");
+            let option4 = document.createElement("option");
+            let option5 = document.createElement("option");
+            let option6 = document.createElement("option");
+            let option7 = document.createElement("option");
+            let option8 = document.createElement("option");
+
+            select.setAttribute("aria-label","Size");
+            option1.textContent = "Select size";
+            option1.setAttribute("value","");
+            option2.textContent = "2XS - Chest 34";
+            option2.setAttribute("value","111")
+            option3.textContent = "XS - Chest 36";
+            option3.setAttribute("value","222")
+            option4.textContent = "S - Chest 38";
+            option4.setAttribute("value","333")
+            option5.textContent = "M - Chest 40";
+            option5.setAttribute("value","444")
+            option6.textContent = "L - Chest 42";
+            option6.setAttribute("value","555")
+            option7.textContent = "XL - Chest 44 - Out of stock";
+            option7.setAttribute("value","666")
+            option8.textContent = "2XL - Chest 46-48 - Out of stock";
+            option8.setAttribute("value","666")
+
+            select.append(option1,option2,option3,option4,option5,option6,option7,option8);
+            div3.append(select);
+
+            let buttonbag = document.createElement("button");
+            buttonbag.textContent = "MOVE TO BAG";
+            buttonbag.setAttribute("class","buttonbag");
+
+            div1.append(image,span,p1,p2,p3,div3,buttonbag);
+            div.append(div1);
+
+            
+        })
+        
+        document.querySelector(".container").append(div);
+    }
+}
+
+
+
+
+/* <div class="favFull">
+    <div>
+        <img>
+            <button></button>
+        </img>
+        <p></p>  des
+        <p></p>  price
+        <p></p>  perfect
+        <div>
+            <select aria-label="Size">
+                <option value="">Select size</option>
+                <option value="202523002">2XS - Chest 34</option>
+                <option value="202523006">XS - Chest 36</option>
+                <option value="202523007">S - Chest 38</option>
+                <option value="202523001">M - Chest 40</option>
+                <option value="202523005">L - Chest 42</option>
+                <option value="202523000">XL - Chest 44 - Out of stock</option>
+                <option value="202523004">2XL - Chest 46-48 - Out of stock</option>
+            </select>
+            <span class="indicator__5B7Y"></span>
+        </div>
+        <button class="buttonbag">Move to bag</button>
+    </div>
+</div> */
+
+        // <div class="FavEmpty">
+        //     <div>
+        //         <span class="material-symbols-outlined">
+        //             favorite
+        //         </span>
+        //         <h1>You have no Saved Items</h1>
+        //         <div>
+        //             <p>Sign in to sync your Saved Items</p> 
+        //             <p>across all your devices.</p>
+        //         </div>
+        //         <a href="./LogIn.html">SIGN IN</a>
+        //     </div>
+        // </div>
+
+
+        
---
+++ MensSection.js
@@ -0,0 +1,200 @@
+let ProductArr = [
+
+    {
+      "image":"https://images.asos-media.com/products/topman-borg-jacket-in-rust/200229300-1-brown?$n_320w$&wid=317&fit=constrain",
+      "des":"Topman borg jacket in rust",
+      "price":"69.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/nike-blazer-mid-77-jumbo-trainers-in-white-and-black/24280232-1-white?$n_320w$&wid=317&fit=constrain",
+      "des":"Nike Blazer Mid '77 Jumbo trainers in white and black",
+      "price":"99.95",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/jack-jones-intelligence-tapered-cargo-trouser-in-khaki/203681776-1-dustyolive?$n_320w$&wid=317&fit=constrain",
+      "des":"Jack & Jones Intelligence tapered cargo trouser in khaki",
+      "price":"48.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/asos-dark-future-oversized-hoodie-in-faux-fur-with-logo-embroidery-in-light-brown/202522994-1-lightbrown?$n_320w$&wid=317&fit=constrain",
+      "des":"ASOS Dark Future oversized hoodie in faux fur with logo embroidery in light brown",
+      "price":"45.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/collusion-joggers-in-charcoal/202058569-1-charcoal?$n_320w$&wid=317&fit=constrain",
+      "des":"COLLUSION joggers in charcoal",
+      "price":"16.95",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/jordan-jumpman-mini-logo-fleece-hoodie-in-black/202278164-1-black?$n_320w$&wid=317&fit=constrain",
+      "des":"Jordan Jumpman mini logo fleece hoodie in black",
+      "price":"64.95",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/new-look-long-sleeve-poplin-shirt-in-black/202093760-1-black?$n_320w$&wid=317&fit=constrain",
+      "des":"New Look long sleeve poplin shirt in black",
+      "price":"13.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/asos-design-lightweight-bomber-jacket-in-black/201560664-1-black?$n_320w$&wid=317&fit=constrain",
+      "des":"ASOS DESIGN lightweight bomber jacket in black",
+      "price":"30.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/asos-design-spray-on-jeans-in-power-stretch-denim-in-black/20344406-1-black?$n_320w$&wid=317&fit=constrain",
+      "des":"ASOS DESIGN spray on jeans in power stretch denim in black",
+      "price":"21.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/asos-design-classic-rigid-jeans-in-black/20313252-1-black?$n_320w$&wid=317&fit=constrain",
+      "des":"ASOS DESIGN classic rigid jeans in blackt",
+      "price":"27.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/nike-unisex-long-sleeve-graphic-tee-with-arm-print-in-black/202967299-1-black?$n_320w$&wid=317&fit=constrain",
+      "des":"Nike Unisex long sleeve graphic tee with arm print in black",
+      "price":"32.95",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/the-north-face-norm-cap-in-black/202553950-1-tnfblack?$n_320w$&wid=317&fit=constrain",
+      "des":"The North Face Norm cap in black",
+      "price":"21.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/new-balance-unisex-members-club-hoodie-in-ecru/203118806-1-ecru?$n_320w$&wid=317&fit=constrain",
+      "des":"New Balance Unisex members club hoodie in ecru",
+      "price":"42.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/nike-utility-cargo-trousers-in-khaki/202298928-1-khaki?$n_320w$&wid=317&fit=constrain",
+      "des":"Nike utility cargo trousers in khaki",
+      "price":"59.95",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/puma-ca-pro-suede-trainers-in-washed-grey-exclusive-to-asos/202576868-1-vaporousgray?$n_320w$&wid=317&fit=constrain",
+      "des":"Puma CA Pro suede trainers in washed grey - exclusive to ASOS",
+      "price":"79.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/new-balance-574-trainers-in-grey/202381835-1-grey?$n_320w$&wid=317&fit=constrain",
+      "des":"New Balance 574 trainers in grey",
+      "price":"75.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/adidas-originals-centennial-trainers-in-white-and-green/203074711-1-white?$n_320w$&wid=317&fit=constrain",
+      "des":"adidas Originals Centennial trainers in white and green",
+      "price":"90.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/puma-training-retaliate-trainers-in-black/14655087-1-black?$n_320w$&wid=317&fit=constrain",
+      "des":"Puma Training Retaliate trainers in black",
+      "price":"58.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/olaplex-no7-bond-oil-1oz-30ml/20626397-1-no7?$n_320w$&wid=317&fit=constrain",
+      "des":"Olaplex No.7 Bond Oil 1oz/30ml",
+      "price":"29.40",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    {
+      "image":"https://images.asos-media.com/products/adidas-originals-adicolor-70s-unisex-striped-track-bottom-in-brown/202829184-1-brown?$n_320w$&wid=317&fit=constrain",
+      "des":"adidas Originals 'adicolor 70s' unisex striped track bottom in brown",
+      "price":"65.00",
+      "favImage":"https://cdn-icons-png.flaticon.com/512/2031/2031035.png"
+    },
+    
+    
+    ];
+console.log(ProductArr)
+displayItems(ProductArr);
+
+function displayItems(Data){
+    document.getElementById("container").innerHTML = null;
+
+    Data.forEach((item,index)=>{
+        let div = document.createElement("div");
+        let image = document.createElement("img");
+        image.src = item.image;
+
+        let p = document.createElement("p");
+        p.textContent = item.des;
+
+        let div1 = document.createElement("div");
+
+        let p1 = document.createElement("p");
+        p1.textContent = "£"+item.price;
+
+        let btn = document.createElement("img");
+        btn.src = item.favImage;
+        btn.addEventListener("click",function(){
+          ProductArr[index].favImage = "https://cdn-icons-png.flaticon.com/512/2030/2030957.png";
+          displayItems(ProductArr);
+          console.log(ProductArr)
+
+            clicked(item,index);
+        });
+
+        div1.append(p1,btn);
+        div.append(image,p,div1);
+        document.getElementById("container").append(div);
+
+    })
+}
+
+let favArr = JSON.parse(localStorage.getItem("Fav-Item"))||[];
+
+function clicked(item,index){
+
+    let status = true;
+    favArr.forEach((element,index)=>{
+        if(item.image == element.image) status = false;
+    })
+
+    if(status){
+        favArr.push(item);
+        localStorage.setItem("Fav-Item",JSON.stringify(favArr));
+    }
+    else{
+      alert("Item already in the Favourites!");
+    }
+}
+
+document.querySelector(".dropdown-content>a:first-child").addEventListener("click",function(){
+        displayItems(ProductArr);
+})
+
+document.querySelector(".dropdown-content>a:nth-child(2)").addEventListener("click",function(){
+  displayItems(ProductArr);
+})
+
+document.querySelector(".dropdown-content>a:nth-child(3)").addEventListener("click",function(){
+  let h = [...ProductArr];
+
+  h.sort((a,b)=>{return b.price - a.price})
+  displayItems(h);
+})
+
+document.querySelector(".dropdown-content>a:nth-child(4)").addEventListener("click",function(){
+  let l = [...ProductArr];
+
+  l.sort((a,b)=>{return a.price - b.price})
+  displayItems(l);
+})
